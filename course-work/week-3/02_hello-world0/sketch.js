9722a6849b7d4ce88c2b5268d77a335164556df3
Frederick Cantarine <fcantarine@gmail.com>
2018-03-12 16:44:15 -0600

    Adding course work p5 examples and committing hw-3

---
+++ course-work/week-3/02_hello-world0/sketch.js
@@ -0,0 +1,7 @@
+function setup(){
+
+}
+
+function draw(){
+      ellipse(50, 60, 70, 80);
+}
---
+++ hw-3/sketch.js
@@ -0,0 +1,16 @@
+//Declaring the setup function
+function setup(){
+
+  // creating a canvas 500 px wide and 300px high
+  createCanvas( 500, 300 );
+
+  //declaring the background green
+  background( 'green' );
+}
+//Declaring the draw function
+function draw(){
+
+  //using the text function, haveing p5 write, "Hello World!"
+  text( "Hello World!", 250, 150 );
+
+}
